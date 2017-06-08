0954c4c4cdad6a18be06c27a67193b04832ea5b6
StillMas <llzem@126.com>
2017-06-08 23:27:22 +0800

    123

---
+++ qz6_9/Notice.scala
@@ -0,0 +1,5 @@
+/** This is a notice
+*   codes in this repository are ready for Spark quiz
+*   Anyone without permission have no right to use any code in this repository
+*   OK now you can leave. bye.
+*/
