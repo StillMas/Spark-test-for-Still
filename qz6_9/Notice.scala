f2654054e3da80143946cd254cba393160e3d347
StillMas <llzem@126.com>
2017-06-09 13:19:05 +0800

    delete

--- qz6_9/Notice.scala
+++
@@ -1,5 +0,0 @@
-/** This is a notice
-*   codes in this repository are ready for Spark quiz
-*   Anyone without permission have no right to use any code in this repository
-*   OK now you can leave. bye.
-*/
