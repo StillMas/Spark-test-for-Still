7af18aa27e79ad22300f53fcb6507baa892a626c
StillMas <llzem@126.com>
2017-06-09 10:45:04 +0800

    Create MLE_l.scala

--- MLE_l.scala
+++ MLE_l.scala
@@ -1,3 +1,6 @@
+//csv file is already imported into HDFS
+val data=sc.textFile("/user/2016210876/Data.csv")
+val data1=data.map(line=>line.split(",")).map(i=>i(1)).map(_.toDouble)
 //use MLE to estimate parameters mu and theta, given that data~U(mu-theta/2,mu+theta/2)
 //according to MLE, mu=(max(x)+min(x))/2,and theta=max(x)-min(x)
 val max_x=data1.reduce((x,y)=>if(x>y) x else y)
